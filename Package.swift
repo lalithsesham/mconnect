21cc28acf935d3fd2ec39e082fcc78c9ca55c629
Lalith <lalith.sesham@privafy.com>
2021-12-01 16:59:57 +0530

    name correction

--- Package.swift
+++ Package.swift
@@ -4,12 +4,12 @@
 import PackageDescription
 
 let package = Package(
-    name: "PfyAeSDK",
+    name: "PfyMcSDK",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "PfyAeSDK",
-            targets: ["PfyAeSDK"]),
+            name: "PfyMcSDK",
+            targets: ["PfyMcSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
-            name: "PfyAeSDK",
+            name: "PfyMcSDK",
             url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1/PfyMcSDK.xcframework.zip",
             checksum: "62e87a6f8a30b18c3b011c9d0b641abc99240a812278ddd9ea2e76a635d5a398"
         )
