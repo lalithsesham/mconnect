2231f0e047f92c982b67d68b66ec83a1f8e69897
Lalith <lalith.sesham@privafy.com>
2021-12-05 08:26:18 +0530

    adding compiled dep fmwk

--- Package.swift
+++ Package.swift
@@ -23,8 +23,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "PfyMcSDK",
-            url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.3/PfyMcSDK.xcframework.zip",
-            checksum: "40f836d18195c98ff499a5f81e8e4c861bc182a675f1a5d0e4ac64729c49846e"
+            //url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.3/PfyMcSDK.xcframework.zip",
+            //checksum: "40f836d18195c98ff499a5f81e8e4c861bc182a675f1a5d0e4ac64729c49846e"
+            url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.6/PfyMcSDK.xcframework.zip",
+	    checksum: "141990d070b95c3887e6824a5503a164baffd5a831381aa61f22b2e1f84a67f7"
             //url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.2/PfyMcSDK.xcframework.zip",
             //checksum: "69188f398fcf4a827e404291a37a479647cd6b50ae90945f66fb53d919df2410"
         )
