ac43cf5de672c23c30c9ddfa4591ac4655a729ff
Lalith <lalith.sesham@privafy.com>
2021-12-06 06:45:42 +0530

    with wrapper

--- Package.swift
+++ Package.swift
@@ -9,35 +9,39 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "PfyMcSDK",
-            targets: ["PrivafyConnectSDK"]),
+            targets: ["PfyMcSDKWrapper", "PfyMcSDK"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-	.package( url: "https://github.com/datatheorem/TrustKit",
-	.exact("2.0.0")
-	)
+        
+        .package(
+            name : "TrustKit",
+            url: "https://github.com/datatheorem/TrustKit",
+                  .exact("2.0.0")
+        )
         
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-	.target(
-	    name: "PrivafyConnectSDK",
-	    dependencies: [
-                .package( url: "https://github.com/datatheorem/TrustKit", .exact("2.0.0"))
-                .target(name: "PfyMcSDK")    
+        .target(
+            name: "PfyMcSDKWrapper",
+            dependencies: [
+                .product( name: "TrustKit", package: "TrustKit"),
+                .target(name: "PfyMcSDK")
             ],
-	),
+            path :"PfyMcSDKWrapper"
+        ),
         .binaryTarget(
             name: "PfyMcSDK",
             //url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.3/PfyMcSDK.xcframework.zip",
             //checksum: "40f836d18195c98ff499a5f81e8e4c861bc182a675f1a5d0e4ac64729c49846e"
             url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.6/PfyMcSDK.xcframework.zip",
-	    checksum: "141990d070b95c3887e6824a5503a164baffd5a831381aa61f22b2e1f84a67f7"
+            checksum: "141990d070b95c3887e6824a5503a164baffd5a831381aa61f22b2e1f84a67f7"
             //url: "https://cirrus-deb-repo.s3.amazonaws.com/aesdk/0.1.2/PfyMcSDK.xcframework.zip",
             //checksum: "69188f398fcf4a827e404291a37a479647cd6b50ae90945f66fb53d919df2410"
         )
-	
+        
         
     ]
 )
---
+++ PfyMcSDKWrapper/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Lalith on 06/12/21.
+//
+
+import Foundation
